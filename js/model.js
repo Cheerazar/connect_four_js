fe096750a35ca7a43b14415f0b798e37177a943f
Gunnari Auvinen <gauvinen@gmail.com>
2014-08-14 12:20:16 -0700

    Add Cell constructor and update makeBoard.

--- js/model.js
+++ js/model.js
@@ -2,16 +2,25 @@ var Board = function () {
   this.gameBoard = this.makeBoard()
 };
 
+var Cell = function() {
+  this.empty = true;
+  this.color = null;
+}
+
 Board.prototype = {
-  makeBoard: function() {
+  makeBoard: function () {
     var board = {}
     for (var i = 0; i < 6; i++) { // row
       board['row' + i] = {};
       for (var j = 0; j < 7; j++) { // column
-        board['row' + i]['col' + j] = {empty: true, color: null};
+        board['row' + i]['col' + j] = new Cell();
       }
     }
     return board;
+  },
+
+  gameWon: function () {
+
   }
 }
 var board = new Board();
