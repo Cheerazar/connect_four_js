c556dc793267b554105590a19b4f7bb77879a44e
Cassidy Clawson & Cute & Cuddly <pair+cassidyclawson.yohanyi@devbootcamp.com>
2014-08-14 16:22:21 -0700

    completed controller and driver code for event listener

---
+++ js/view.js
@@ -0,0 +1,41 @@
+// PLACEHOLDER MODEL
+var Model = function() {
+
+};
+
+Model.prototype = {
+  dropInColumn: function(col) {
+    console.log(col); }
+}
+
+// VIEW
+var View = function(){
+  };
+
+View.prototype = {
+// Next steps are to update the view with data from the model.
+};
+
+//CONTROLLER
+
+var Controller = function(model,view) {
+};
+
+Controller.prototype = {
+  whichColumn: function() {
+    model.dropInColumn(parseInt($(this).attr('class')[3], 10));
+    // console.log(parseInt($(this).attr('class')[3]));
+  },
+  initializeEvents: function() {
+    for ( var i = 1; i <= 7; i++) {
+    $("td").on('click', this.whichColumn);
+  }
+}
+};
+
+// DRIVER CODE
+
+var model = new Model();
+var view = new View();
+var controller = new Controller(model, view);
+controller.initializeEvents();
